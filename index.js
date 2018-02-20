107febdd59f8eafad6e33f1563307430f1954bd0
hideokamoto <hide.okamoto@digitalcube.jp>
2018-02-20 13:06:51 +0900

    update iam & fix libs

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Alexa = require('alexa-sdk')
 const handlers = require('./lib/handlres')
 
 module.exports.handler = function (event, context, callback) {
-  var alexa = Alexa.handler(event, context)
+  const alexa = Alexa.handler(event, context)
+  alexa.appId = 'amzn1.ask.skill.f35b1223-1ed1-4283-a207-0e5d0fd2ff05'
   alexa.registerHandlers(handlers)
   alexa.execute()
 }
